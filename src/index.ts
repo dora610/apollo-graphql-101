e608a603639d3ae1c6ca0eaf9088df0b06b21f97
“Surajit <surajitkaruri@gmail.com>
2023-07-18 00:03:50 +0530

    added basic resolvers

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+import { typeDefs } from './schema.js'
+
+// db
+import db from './_db.js';
+
+const { games, authors, reviews } = db;
+
+const resolvers = {
+    Query: {
+        reviews: ()=> reviews,
+        review: (_, args) => reviews.find((review) => review.id === args.id),
+        games: ()=> games,
+        game: (_, args) => games.find((game) => game.id === args.id),
+        authors: ()=> authors,
+        author: (_, args) => authors.find((author) => author.id === args.id)
+    },
+    Game: {
+        reviews: (parent) => reviews.filter((review) => review.game_id === parent.id)
+    },
+    Author: {
+        reviews: (parent) => reviews.filter((review) => review.author_id === parent.id)
+    },
+    Review: {
+        game: (parent) => games.find((game) => game.id === parent.game_id),
+        author: (parent) => authors.find((author) => author.id === parent.author_id),
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 }
+})
+
+console.log(`🚀  Server ready at: ${url}`);
